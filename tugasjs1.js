1bf10ad488bcd2572f8fcfefa763149b33a42e8e
aditrmlyna <aditriamulyana@gmail.com>
2023-10-08 16:31:25 +0700

    yes

---
+++ tugasjs1.js
@@ -0,0 +1,64 @@
+// if, Else, Nested If
+// If, Else
+// let buku = 2;
+// if (buku == 1) {
+//   console.log("Buku Cinta Brontosaurus");
+// } else {
+//   console.log("Buku Marmut Merah Jambu");
+// }
+
+// Nested If
+// let tim = "arsenal";
+// let pemain = "bukayo saka";
+// if (tim == "arsena") {
+//   if (pemain == "bukayo saka") {
+//     console.log("Ya, dia pemain arsenal!");
+//   } else {
+//     console.log("Salah, dia buka pemain arsenal!");
+//   }
+// } else {
+//   console.log("tim ini tidak dikenali!");
+// }
+
+// Switch Case
+// let paket = 1;
+// mytic = 25000;
+// legend = 20000;
+// epic = 15000;
+// switch (paket) {
+//   case 1:
+//     console.log("Anda membeli paket ayam MYTIC dengan harga Rp." + " " + mytic);
+//     break;
+//   case 2:
+//     console.log("Anda membeli paket ayam LEGEND dengan harga Rp." + " " + legend);
+//     break;
+//   default:
+//     console.log("Anda membeli paket ayam EPIC dengan harga Rp." + " " + epic);
+// }
+
+// For
+// for (let i = 1; i <= 30; i += 2) {
+//   console.log("Ini Bilangan Ganji Ke-" + i + "");
+// }
+
+// while
+// let i = 0;
+// while (i <= 20) {
+//   console.log("Ini Bilangan Genap Ke-" + " " + i);
+//   i += 2;
+// }
+
+// Do While
+// let i = 0;
+// while (i < 10) {
+//   console.log("Arsenal otw juara ucl!");
+//   i++;
+// }
+
+//function
+let vb = volumebalok(10, 20, 5);
+console.log("Jadi volume baloknya adalah" + " " + vb);
+
+function volumebalok(p, l, t) {
+  return p * l * t;
+}
